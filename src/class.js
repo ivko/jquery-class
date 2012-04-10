51526cddad4120a2a8d3d7b24b5368367b4a668e
gcoguiec <gcoguiec@idealtechnology.net>
2012-04-10 16:23:42 -0400

    Add the bind method to the Function prototype plus make the code trendy again.

--- src/class.js
+++ src/class.js
@@ -1,4 +1,6 @@
-(function($, window) {
+!function($, window) {
+
+    "use strict";
 
     var enumerables = true, slice = Array.prototype.slice, has = Object.prototype.hasOwnProperty;
     for (var i in { toString: 1 }) { enumerables = null; }
@@ -19,29 +21,49 @@
         };
     };
 
+    window.Function.prototype.bind = function(that) {
+        var self = this,
+            args = arguments.length > 1 ? Array.slice(arguments, 1) : null,
+            F = function(){};
+
+        var bound = function(){
+            var context = that, length = arguments.length;
+            if (this instanceof bound){
+                F.prototype = self.prototype;
+                context = new F();
+            }
+            var result = (!args && !length) ? self.call(context)
+                : self.apply(context, args && length ? args.concat(Array.slice(arguments)) : args || arguments);
+            return context == that ? result : context;
+        };
+        return bound;
+    };
+
     $.Class = function(params) {
         if ('undefined' === typeof params) { return $.noop; }
         params = ($.isFunction(params)) ? { initialize: params } : params;
-        this['$instance'] = function() { return (this.initialize) ? this.initialize.apply(this, arguments) : this; };
-        var $instance = this['$instance']; // hack to prevent chrome inspector lexer to display internal var...
-        delete this['$instance'];
+        this.$instance = function() { return (this.initialize) ? this.initialize.apply(this, arguments) : this; };
+        var $instance = this.$instance; // hack to prevent chrome inspector lexer to display internal var...
+        delete this.$instance;
         $instance.extend = function(key, value) { this.prototype[key] = value; }.overload();
         $instance.prototype.implement = function(key, value) { this[key] = value; }.overload();
+
         // Extends
         if (has.call(params, "Extends")) {
             if ($.isFunction(params.Extends)) {
                 $instance.extend(params.Extends.prototype);
                 $instance.extend('parent', params.Extends.prototype); // unique parent
-            } else if ($.isArray(params.Extends)) for(e in params.Extends) $instance.extend(params.Extends[e].prototype);
+            } else if ($.isArray(params.Extends)) for(var e in params.Extends) $instance.extend(params.Extends[e].prototype);
         }
         delete $instance.prototype.Extends;
         $.extend($instance.prototype, params);
+
         // Mutators
-        for(m in $.Class.Mutators) $.Class.Mutators[m].call($instance, has.call(params, m) ? params[m] : ($.Class.MutatorsDefaults.indexOf(m) >= 0));
+        for(var m in $.Class.Mutators) $.Class.Mutators[m].call($instance, has.call(params, m) ? params[m] : ($.Class.MutatorsDefaults.indexOf(m) >= 0));
         return $instance;
     };
 
     $.Class.Mutators = {};
     $.Class.MutatorsDefaults = [];
 
-})(jQuery, this);
+}(this.jQuery, this);
